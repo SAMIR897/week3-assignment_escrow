d4499e18ffccb3b0e647e33e2351f5826658d645
SAMIR897 <samirkuimar708@gmail.com>
2026-05-18 15:03:44 +0530

    final project locks and configs

---
+++ update_git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git reset HEAD~1
+git add Anchor.toml Cargo.toml package.json tsconfig.json .gitignore .prettierignore migrations/
+git commit -m "initial anchor scaffolding"
+git add programs/
+git commit -m "wrote the escrow smart contract logic"
+git add tests/
+git commit -m "added typescript tests for the instructions"
+git add README.md tests-screenshot*.png
+git commit -m "added readme and screenshots of passing tests"
+git add .
+git commit -m "final project locks and configs"
+git push -f -u origin main
